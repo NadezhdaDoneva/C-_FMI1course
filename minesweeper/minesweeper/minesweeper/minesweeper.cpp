02723a6e61d89487cd9eed995dd3f19ac29ac9c4
NadezhdaDoneva <donevanad@gmail.com>
2023-12-24 10:14:48 +0200

    placing mines randomly functionallity added

--- minesweeper/minesweeper/minesweeper/minesweeper.cpp
+++ minesweeper/minesweeper/minesweeper/minesweeper.cpp
@@ -6,9 +6,9 @@
 #include <time.h> 
 using namespace std;
 
-bool validationOfInput(int boardDimension, int mines) {
+bool validationOfInput(size_t boardDimension, unsigned minesCount) {
     int maxMines = 3 * boardDimension;
-    if (boardDimension >= 3 && boardDimension <= 10 && mines >= 1 && mines <= maxMines)
+    if (boardDimension >= 3 && boardDimension <= 10 && minesCount >= 1 && minesCount <= maxMines)
     {
         return 1;
     }
@@ -17,28 +17,58 @@ bool validationOfInput(int boardDimension, int mines) {
     }
 }
 
-void customizeSettings(int& boardDimension, int& mines) {
+void customizeSettings(size_t& boardDimension, unsigned& minesCount) {
     cout << "Enter board dimensions: ";
     cin >> boardDimension;
     cout << "Enter number of mines: ";
-    cin >> mines;
+    cin >> minesCount;
 
-    while (!validationOfInput(boardDimension, mines))
+    while (!validationOfInput(boardDimension, minesCount))
     {
         cout << "Invalid input, please try again" << endl;
         cout << "Enter board dimensions: ";
         cin >> boardDimension;
         cout << "Enter number of mines: ";
-        cin >> mines;
+        cin >> minesCount;
     }
 }
 
-void placeMinesRandomly(char realBoard[10][10], int minesCoordinatesArr[30][2]) {
-    srand(time(0));
-
+//check if mine with the same coordinates is already generated
+bool isRepeatedMine(int minesCoordinatesArr[30][2], int& x, int& y, unsigned minesCount) {
+    for (size_t i = 0; i < minesCount; i++)
+    {
+        if (minesCoordinatesArr[i][0] == x && minesCoordinatesArr[i][1] == y)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
-void createBoard(char realBoard[10][10], char playerBoard[10][10], int boardDimension, int mines, int minesCoordinatesArr[30][2]) {
+void placeMinesRandomly(char realBoard[10][10], int minesCoordinatesArr[30][2], unsigned minesCount, size_t boardDimension) {
+    srand(time(0));
+    int x = 0;
+    int y = 0;
+
+    for (size_t i = 0; i < minesCount; i++)
+    {
+        x = rand() % boardDimension;
+        y = rand() % boardDimension;
+        x = 2;
+        y = 2;
+        while (isRepeatedMine(minesCoordinatesArr, x, y, minesCount))
+        {
+            x = rand() % boardDimension;
+            y = rand() % boardDimension;
+        }
+        cout << x << y << endl;
+        minesCoordinatesArr[i][0] = x;
+        minesCoordinatesArr[i][1] = y;
+        realBoard[x][y] = '*';
+    }
+}
+
+void createBoard(char realBoard[10][10], char playerBoard[10][10], size_t boardDimension, unsigned minesCount, int minesCoordinatesArr[30][2]) {
     for (int i = 0; i < boardDimension; i++)
     {
         for (int j = 0; j < boardDimension; j++)
@@ -47,26 +77,24 @@ void createBoard(char realBoard[10][10], char playerBoard[10][10], int boardDime
             playerBoard[i][j] = '_';
         }
     }
-    placeMinesRandomly(realBoard, minesCoordinatesArr);
+    placeMinesRandomly(realBoard, minesCoordinatesArr, minesCount, boardDimension);
 }
 
-void playMinesweeper(char realBoard[10][10], char playerBoard[10][10], int boardDimension, int mines) {
+void playMinesweeper(char realBoard[10][10], char playerBoard[10][10], size_t boardDimension, unsigned minesCount) {
     int minesCoordinatesArr[30][2];
-    createBoard(realBoard, playerBoard, boardDimension, mines, minesCoordinatesArr);
+    createBoard(realBoard, playerBoard, boardDimension, minesCount, minesCoordinatesArr);
 
     bool gameOver = false;
 }
 
 int main()
 {
-    int boardDimension = 0;
-    int mines = 0;
-    customizeSettings(boardDimension, mines);
+    size_t boardDimension = 0;
+    unsigned minesCount = 0;
+    customizeSettings(boardDimension, minesCount);
     char realBoard[10][10];
     char playerBoard[10][10];
-    playMinesweeper(realBoard, playerBoard, boardDimension, mines);
+    playMinesweeper(realBoard, playerBoard, boardDimension, minesCount);
 
-    srand(time(0));
-    cout << rand() % boardDimension * boardDimension;
 }
 
